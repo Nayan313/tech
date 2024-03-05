d1aaca4c061e179e4c43ae3824026efde23b514c
Nayan313 <113910121+Nayan313@users.noreply.github.com>
2024-03-05 23:07:40 +0530

    beta 1.0

---
+++ js/product.js
@@ -0,0 +1,82 @@
+const getPage = (b) => {
+    console.log("hello from product script"+b);
+
+
+    const productData = [
+        {
+          productId: "mss24u",
+          name: "galaxy s24 ultra",
+          price: "₹111111,39,999",
+          img: "photo/mobile/s24ultra.png",
+        },
+        {
+          productId: "mi15p",
+          name: "iphone 15 pro max",
+          price: "₹1,59,999",
+          img: "photo/mobile/15pro.png",
+        },
+        {
+          productId: "mss24p",
+          name: "galaxy s24 plus",
+          price: "₹1,00,999",
+          img: "photo/mobile/24plus.png",
+        },
+        {
+          productId: "msfo5",
+          name: "galaxy fold 5",
+          price: "₹1,69,999",
+          img: "photo/mobile/fold 5.png",
+        },
+        {
+          productId: "msfi5",
+          name: "galaxy flip 5",
+          price: "₹1,00,999",
+          img: "photo/mobile/flip 5.png",
+        },
+        {
+          productId: "mgp8p",
+          name: "pixel 8 pro",
+          price: "₹85,999",
+          img: "photo/mobile/8pro.png",
+        },
+        {
+          productId: "mi15",
+          name: "iphone 15",
+          price: "₹79,999",
+          img: "photo/mobile/15.png",
+        },
+        {
+          productId: "ms73",
+          name: "samsung A73",
+          price: "₹39,999",
+          img: "photo/mobile/a73.png",
+        },
+        {
+          productId: "mi12",
+          name: "iphone 12",
+          price: "₹49,999",
+          img: "photo/mobile/iphone 12.png",
+        },
+        {
+          productId: "mop12p",
+          name: "one plus 12 pro",
+          price: "₹79,999",
+          img: "photo/mobile/one plus 12.png",
+        },
+        {
+          productId: "mss24",
+          name: "samsung s24",
+          price: "₹89,999",
+          img: "photo/mobile/s24 (2).png",
+        },
+        {
+          productId: "mv100x",
+          name: "vivo 100x",
+          price: "₹59,999",
+          img: "photo/mobile/vivo 100x.png",
+        },
+      ];
+      
+    
+
+}
--- js/shopscript.js
+++ js/shopscript.js
@@ -11,7 +11,6 @@ categoryBoxes.forEach(function (categoryBox) {
     const a = clickedBox.id;
     getGraph(a);
 
-    console.log(a);
     clickedBox.classList.add("active");
   });
 });
@@ -19,78 +18,79 @@ categoryBoxes.forEach(function (categoryBox) {
 
 
 const getGraph = (a) => {
-  console.log(a + 1);
-
+  
   const mobile = [
     {
+      productId: "mss24u",
       name: "galaxy s24 ultra",
       price: "₹1,39,999",
       img: "photo/mobile/s24ultra.png",
     },
     {
+      productId: "mi15p",
       name: "iphone 15 pro max",
       price: "₹1,59,999",
       img: "photo/mobile/15pro.png",
     },
     {
+      productId: "mss24p",
       name: "galaxy s24 plus",
       price: "₹1,00,999",
       img: "photo/mobile/24plus.png",
     },
     {
+      productId: "msfo5",
       name: "galaxy fold 5",
       price: "₹1,69,999",
       img: "photo/mobile/fold 5.png",
     },
     {
+      productId: "msfi5",
       name: "galaxy flip 5",
       price: "₹1,00,999",
       img: "photo/mobile/flip 5.png",
     },
     {
-      name: "galaxy s24",
-      price: "₹89,999",
-      img: "photo/mobile/s24.png",
-    },
-    {
+      productId: "mgp8p",
       name: "pixel 8 pro",
       price: "₹85,999",
       img: "photo/mobile/8pro.png",
     },
     {
+      productId: "mi15",
       name: "iphone 15",
       price: "₹79,999",
       img: "photo/mobile/15.png",
     },
     {
+      productId: "ms73",
       name: "samsung A73",
       price: "₹39,999",
       img: "photo/mobile/a73.png",
     },
     {
+      productId: "mi12",
       name: "iphone 12",
       price: "₹49,999",
       img: "photo/mobile/iphone 12.png",
     },
     {
+      productId: "mop12p",
       name: "one plus 12 pro",
       price: "₹79,999",
       img: "photo/mobile/one plus 12.png",
     },
     {
+      productId: "mss24",
       name: "samsung s24",
       price: "₹89,999",
       img: "photo/mobile/s24 (2).png",
     },
     {
+      productId: "mv100x",
       name: "vivo 100x",
       price: "₹59,999",
       img: "photo/mobile/vivo 100x.png",
-    },
-    {
-      name: "iphone 15",
-      price: "₹79,999",
-      img: "photo/mobile/15.png",
     },
   ];
 
@@ -344,7 +344,7 @@ const getGraph = (a) => {
   // Generate HTML cards for the selected data
   data.forEach(function (item) {
     cardOp += `
-          <div class="shop-card" title="${item.name}">
+          <div class="shop-card" title="${item.name}" id="${item.name}">
               <div class="shop-img-sec">
                   <img src="${item.img}" alt="">
               </div>
@@ -356,15 +356,30 @@ const getGraph = (a) => {
 
   // Update card section with generated HTML
   cardSection.innerHTML = cardOp;
-};
-let shopCard = document.querySelector(".shop-card");
-function twoRow(){
-shopCard.style.minWidth="330px"
-shopCard.style.maxWidth="430px"
-}
-function threeRow(){
+
+
+
+  let shopBoxes = document.querySelectorAll(".shop-card");
+
+  shopBoxes.forEach(function (shopBox) {
+    shopBox.addEventListener("click", function (event) {
+      let clickedBox = event.currentTarget;
+      const b = clickedBox.id;
+      getPage(b);
+      console.log(b + 23);
+    });
+  });
+  console.log(123456);
 
 }
-function fourRow(){
+let shopBoxes = document.querySelectorAll(".shop-card");
 
-}
+shopBoxes.forEach(function (shopBox) {
+  shopBox.addEventListener("click", function (event) {
+    let clickedBox = event.currentTarget;
+    const b = clickedBox.id;
+    getPage(b);
+    console.log(b + 23);
+  });
+});
+console.log(123456);
